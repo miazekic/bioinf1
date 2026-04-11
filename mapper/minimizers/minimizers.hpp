f082db538b0367a59fbde86abd26f0e93b14b437
gsengi <gabriela.sunjic@fer.hr>
2026-04-11 19:15:08 +0200

    Minimizer pocetak

--- mapper/minimizers/minimizers.hpp
+++ mapper/minimizers/minimizers.hpp
@@ -1,14 +1,15 @@
 #pragma once
+
 #include <vector>
+#include <tuple>
 #include <string>
 
 namespace mapper {
 
-struct Minimizer {
-    size_t position;
-    std::string kmer;
-};
-
-std::vector<Minimizer> Minimize(const char* sequence, size_t len, int k, int w);
+std::vector<std::tuple<unsigned int, unsigned int, bool>> Minimize(
+    const char* sequence, unsigned int sequence_len,
+    unsigned int kmer_len,
+    unsigned int window_len);
 
 }
+
